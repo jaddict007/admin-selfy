c24c1d96a09ff78149cd122e51175dde5625fbce
Gowtham S <gowthams@hq.microobjects.net>
2015-11-05 14:15:02 +0530

    menu transition and bootstrap table implementation

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,3 +1,20 @@
 $(function(){
 	$("#aside-menu").metisMenu();
 });
+
+$(document).on('click', '#menu-opener', function(){
+	var el = $(this);
+	if(el.hasClass('out')){
+		el.removeClass('out');
+		$('.sidebar').removeClass('sidebar-hide');
+		$('#main').removeClass('main-full');
+	}else{
+		el.addClass('out');
+		$('.sidebar').addClass('sidebar-hide');
+		$('#main').addClass('main-full');
+	}
+	var timer = setTimeout(function(){
+		clearTimeout(timer);
+		$('#table').bootstrapTable('resetView');
+	}, 500);
+});
